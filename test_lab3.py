31f3f3dc25807455fa0aef49b749d6d54e3ef1a3
Yue <yueyue-0929@hotmail.com>
2015-10-29 12:05:17 -0400

    LAB 3 YUE YANG

--- test_lab3.py
+++ test_lab3.py
@@ -13,24 +13,65 @@ MONTHS_WITH_31 = ["January", "March", "May", "July", "August", "October", "Decem
 MONTHS_WITH_30 = ["April", "June", "September", "November"]
 MONTHS_WITH_28_or_29 = ["February"]
 
+MONTHS_WITH_31_LOWER=[month.lower() for month in MONTHS_WITH_31]
+MONTHS_WITH_30_LOWER=[month.lower() for month in MONTHS_WITH_30]
+MONTHS_WITH_28_or_29_LOWER=[month.lower() for month in MONTHS_WITH_28_or_29]
+
 
 def test_months_with_31():
     """
     Test months with 31 days
     """
+    # Write a test function for the months with 31 days
     for item in MONTHS_WITH_31:
         assert days_in_month(item) == 31
 
-# Write a test function for the months with 30 days
+
+def test_months_with_30():
+    # Write a test function for the months with 30 days
+    for item in MONTHS_WITH_30:
+        assert days_in_month(item) == 30
 
 
-# Write a test function for the months with 28 or 29 days
+def test_months_with_28_or_29():
+    # Write a test function for the months with 28 or 29 days
+    for item in MONTHS_WITH_28_or_29:
+        assert days_in_month("February") == "28 or 29"
 
 
-# Write a test function for months that are not capitalized properly
-# Hint: use the lower() string method
+    # Write a test function for months that are not capitalized properly
+    # Hint: use the lower() string method
 
-# Write a test function for unexpected input
-# Hint: use a try/except block to deal with the exception
-# Hint: use data types other than strings as input
+def test_months_with_31_lower():
+    # Write a test function for the months with 31 days
+    for item in MONTHS_WITH_31_LOWER:
+        assert days_in_month(item) == 31
 
+
+def test_months_with_30_lower():
+    # Write a test function for the months with 30 days
+    for item in MONTHS_WITH_30_LOWER:
+        assert days_in_month(item) == 30
+
+
+def test_months_with_28_or_29_lower():
+    # Write a test function for the months with 28 or 29 days
+    for item in MONTHS_WITH_28_or_29_LOWER:
+        assert days_in_month(item) == "28 or 29"
+
+
+    # Write a test function for unexpected input
+    # Hint: use a try/except block to deal with the exception
+    # Hint: use data types other than strings as input
+
+def test_unexpected():
+
+    try:
+        days_in_month("abcde")
+    except ValueError:
+        assert True
+
+    try:
+        days_in_month(9)
+    except AttributeError:
+        assert True
